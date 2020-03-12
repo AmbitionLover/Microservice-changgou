4e0a7261c62d3197230300323199d3657e1a8d63
Ambi <1277518148@qq.com>
2020-03-12 09:34:56 +0800

    first commit

---
+++ changgou-eureka/src/main/java/com/changgou/EurekaApplication.java
@@ -0,0 +1,19 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Ambi
+ * @version 1.0
+ * @date 2020/3/11 20:44
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}
